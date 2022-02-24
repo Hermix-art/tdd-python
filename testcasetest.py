0b4a6d8882f2f2ca22fa9706358dfb0f81758a31
herman_kulik <herman_kulik@epam.com>
2022-02-24 15:21:30 +0100

    delete 'testRunning()' and rename 'testSetUp()'

--- testcasetest.py
+++ testcasetest.py
@@ -5,19 +5,11 @@ class TestCaseTest (TestCase):
         #global instance for current TestCaseTest object
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        #invocation, which changes 'None' to '1'
-        self.test.run()
-
-        #meaning of wasRun attribute is '1'
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert(self.test.log == "setUp testMethod")
 
 
 #  it extends TestCase class. So it has 'run()' method. When we call run below, it invokes, method name provided in constructor
 #  in case of WasRun we provide 'testMethod()', so it invokes it. currently, we provide 'testRunning()'
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
