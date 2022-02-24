3d5d47ccc60d17a5766340de6aa8789cf558c64e
herman_kulik <herman_kulik@epam.com>
2022-02-24 14:50:31 +0100

    move wasRun flag initialization to 'setUp()' method

--- testcasetest.py
+++ testcasetest.py
@@ -3,10 +3,10 @@ from wasrun import TestCase, WasRun
 class TestCaseTest (TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        #meaning of wasRun attribute is 'None'
-        assert(not test.wasRun)
+
         #invocation, which changes 'None' to '1'
         test.run()
+
         #meaning of wasRun attribute is '1'
         assert(test.wasRun)
 
--- wasrun.py
+++ wasrun.py
@@ -3,14 +3,12 @@ from testcase import TestCase
 #WasRun is a subclass of TestCase
 class WasRun(TestCase):
     def __init__(self,name):
-        self.wasRun= None
-        
         # if name is "testMethod" than this method is invoked
         TestCase.__init__(self, name)
-
 
     def testMethod(self):
         self.wasRun= 1
 
     def setUp(self):
+        self.wasRun= None
         self.wasSetUp= 1
