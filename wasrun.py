d22b6375de416838abf94131afbcb141e9e2ace5
herman_kulik <herman_kulik@epam.com>
2022-02-24 11:16:52 +0100

    add parent class, and move initialization there

--- wasrun.py
+++ wasrun.py
@@ -1,10 +1,17 @@
-class WasRun:
-    def __init__(self,name):
-        self.wasRun= None
+class TestCase:
+    def __init__(self, name):
         self.name = name
 
+
+
+class WasRun(TestCase):
+    def __init__(self,name):
+        # if name is "testMethod" than this method is invoked
+        self.wasRun= None
+        TestCase.__init__(self, name)
+
     def run(self):
-        # self.testMethod()
+        # dynamical invocation of "testmethod"
         exec ("self." + self.name + "()")  
 
     def testMethod(self):
