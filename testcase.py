233d001e304b8a86e8989afe3de03de841685ce6
herman_kulik <herman_kulik@epam.com>
2022-02-25 08:43:25 +0100

    add TestResult, and add fake implementation of it

--- testcase.py
+++ testcase.py
@@ -1,5 +1,8 @@
 # when we invoke 'run()' method, it calls 'setUp()' and  invokes <name> method passed in constructor (dynamically)
 
+from testresult import TestResult
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -13,6 +16,11 @@ class TestCase:
 
     def run(self):
         self.setUp()
+
         # dynamical invocation of "testmethod"
-        exec ("self." + self.name + "()")
+        method = getattr(self, self.name)
+        method()
+
         self.tearDown()
+        return TestResult()
+
--- testcasetest.py
+++ testcasetest.py
@@ -1,3 +1,4 @@
+from unittest import result
 from wasrun import TestCase, WasRun
 
 class TestCaseTest (TestCase):
@@ -5,10 +6,15 @@ class TestCaseTest (TestCase):
         test = WasRun("testMethod")
         test.run()
         assert(test.log == "setUp testMethod tearDown")
+    
+    def testResult(self):
+        test = WasRun("testMethod")
+        result= test.run()
+        assert("1 run, 0 failed" == result.summary())
 
 
 #  it extends TestCase class. So it has 'run()' and 'setUp()' methods.
 #  When we call run below, it invokes those methods. 'run()' invokes the method with the name, passed via constructor (testTemplateMethod)
 #  1) create TestCase subclass, providing method name to invoke; 
-#  2) call 'run()' and it will invoke 'setUp()' method (default) first and method, passed via constructor then.
+#  2) call 'run()' and it will invoke default'setUp()' method, method, passed via constructor, and tearDown method (default)
 TestCaseTest("testTemplateMethod").run()
